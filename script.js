67898724230452fcf95a5090cb5ba3f6e3e1342b
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-04-08 16:32:06 +0200

    hw-4 js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let result = 5 + 5 + '5'
-console.log('Type of Data -> Result:', typeof result)
-let email = 'email@gmail.com'
-console.log('User Email:', email)
-console.log('Email have @:', email.includes('@'));
-console.log('Email Lenght:', email.length)
-const wordOne = 'My'
-const wordTwo = 'name'
-const wordThree = 'is'
-let fullName = `${wordOne} ${wordTwo} ${wordThree} Viktor`;
-console.log('Full Name:', fullName)
-let userName = 'Oleksandr'
-let payment = 300
-alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
+let fieldOne = "дз номер 4";
+let fieldTwo = "заповнене поле";
+console.log(fieldOne.trim() !== "" && fieldTwo.trim() !== "" ? "Обидва поля заповнені" : "Не всі поля заповнені");
+
+let num1 = 3;
+let num2 = 6;
+let sum = num1 + num2;
+console.log(sum > 10 ? "Сума більша за 10" : "Сума менша або дорівнює 10");
+
+let text = "Цей код написаний на JavaScript";
+console.log(text.includes("JavaScript") ? "Текст містить слово JavaScript" : "Текст не містить слово JavaScript");
+
+let number = 15;
+console.log((number > 10 && number < 20) ? "Число входить в діапазон від 10 до 20" : "Число не входить в діапазон від 10 до 20");
+
+let name = "Lain";
+let email = "lain@gmail.com";
+let password = "qwerty";
+let emailValid = email.includes("@") && email.indexOf(".") > email.indexOf("@");
+console.log((name.length >= 3 && emailValid && password.length >= 6) ? "Перенаправлення на іншу сторінку" : "Помилка: неправильне заповнення");
