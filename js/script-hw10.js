78c8a0fe8ae8c9817052b158878a1aa04f38cbaf
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-06-03 17:36:40 +0200

    hw10

---
+++ js/script-hw10.js
@@ -0,0 +1,65 @@
+const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
+const words = ['кіт', 'собака', 'миша', 'папуга', 'хомяк'];
+
+function countItems(array, condition) {
+    let count = 0;
+    for (const element of array) {
+        if (condition(element)) {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+const isEven = function(num) {
+    return num % 2 === 0;
+};
+const isLarge = function(num) {
+    return num > 10;
+};
+const isShort = function(word) {
+    return word.length <= 3;
+};
+
+console.log('Парних чисел:', countItems(numbers, isEven));
+console.log('Чисел більше 10:', countItems(numbers, isLarge));
+console.log('Коротких слів:', countItems(words, isShort));
+
+const calculate = function(a, b, operation) {
+    return operation(a, b);
+};
+
+const add = function(a, b) {
+    return a + b;
+};
+const subtract = function(a, b) {
+    return a - b;
+};
+const multiply = function(a, b) {
+    return a * b;
+};
+const divide = function(a, b) {
+    if (b === 0) {
+        return 'Помилка: ділення на нуль';
+    }
+    return a / b;
+};
+
+console.log(calculate(10, 5, add));
+console.log(calculate(10, 5, subtract));
+console.log(calculate(10, 5, multiply));
+console.log(calculate(10, 5, divide));
+console.log(calculate(10, 0, divide));
+
+function repeatMessage(times, messageCreator) {
+    for (let i = 0; i < times; i++) {
+        messageCreator(i);
+    }
+}
+
+repeatMessage(3, function(i) {
+    console.log('Повідомлення №' + (i + 1));
+});
+repeatMessage(2, function(i) {
+    console.log('Hello, this is call #' + (i + 1));
+});
