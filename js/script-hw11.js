0af48753b3bc839d8eeb0f9dc92d9fd8495f520b
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-06-12 21:30:18 +0200

    fix

--- js/script-hw11.js
+++ js/script-hw11.js
@@ -2,35 +2,35 @@ let bankAccount = {
   ownerName: "Anna Shevchenko",
   accountNumber: "95353376547",
   balance: 1000,
-  deposit: function(amount) {
-    this.balance = this.balance + amount
-    alert("Рахунок поповнено на " + amount + ". Залишок: " + this.balance)
+  deposit(amount) {
+    this.balance = this.balance + amount;
+    alert("Рахунок поповнено на " + amount + ". Залишок: " + this.balance);
   },
-  withdraw: function(amount) {
+  withdraw(amount) {
     if (amount > this.balance) {
-      alert("Недостатньо коштів на рахунку")
+      alert("Недостатньо коштів на рахунку");
     } else {
-      this.balance = this.balance - amount
-      alert("Знято " + amount + ". Залишок: " + this.balance)
+      this.balance = this.balance - amount;
+      alert("Знято " + amount + ". Залишок: " + this.balance);
     }
   }
-}
+};
 
 if (confirm("Бажаєте поповнити рахунок?")) {
-  let sum = prompt("Введіть суму для поповнення")
-  sum = Number(sum)
+  let sum = prompt("Введіть суму для поповнення");
+  sum = Number(sum);
   if (!isNaN(sum) && sum > 0) {
-    bankAccount.deposit(sum)
+    bankAccount.deposit(sum);
   } else {
-    alert("Некоректна сума")
+    alert("Некоректна сума");
   }
 } else if (confirm("Бажаєте зняти готівку?")) {
-  let sum = prompt("Введіть суму для зняття")
-  sum = Number(sum)
+  let sum = prompt("Введіть суму для зняття");
+  sum = Number(sum);
   if (!isNaN(sum) && sum > 0) {
-    bankAccount.withdraw(sum)
+    bankAccount.withdraw(sum);
   } else {
-    alert("Некоректна сума")
+    alert("Некоректна сума");
   }
 }
 
@@ -38,47 +38,55 @@ let weather = {
   temperature: 0,
   humidity: 60,
   windSpeed: 10,
-  isBelowZero: function() {
-    return this.temperature < 0
+  checkTemperature(temp) {
+    if (temp < 0) {
+      alert("температура нижче 0 градусів Цельсія: " + temp);
+      return true;
+    } else {
+      alert("температура вище або рівна 0 градусів Цельсія: " + temp);
+      return false;
+    }
   }
-}
+};
 
-weather.temperature = Number(prompt("Введіть температуру повітря"))
-if (weather.isBelowZero()) {
-  alert("температура нижче 0 градусів Цельсія")
+let tempInput = prompt("Введіть температуру повітря");
+tempInput = Number(tempInput);
+if (!isNaN(tempInput)) {
+  weather.temperature = tempInput;
+  weather.checkTemperature(tempInput);
 } else {
-  alert("температура вище або рівна 0 градусів Цельсія")
+  alert("Введіть коректне число температури");
 }
 
 let user = {
   name: "user",
   email: "user_email@example.com",
   password: "12345",
-  login: function(email, pass) {
+  login(email, pass) {
     if (email === this.email && pass === this.password) {
-      alert("Вхід успішний")
+      alert("Вхід успішний");
     } else {
-      alert("Невірний email або пароль")
+      alert("Невірний email або пароль");
     }
   }
-}
+};
 
-let email = prompt("Введіть email")
-let pass = prompt("Введіть пароль")
-user.login(email, pass)
+let email = prompt("Введіть email");
+let pass = prompt("Введіть пароль");
+user.login(email, pass);
 
 let movie = {
   title: "Inception",
   director: "Christopher Nolan",
   year: 2010,
   rating: 8.8,
-  isHighRated: function() {
-    return this.rating > 8
+  isHighRated() {
+    return this.rating > 8;
   }
-}
+};
 
-console.log("Назва:", movie.title)
-console.log("Режисер:", movie.director)
-console.log("Рік:", movie.year)
-console.log("Рейтинг:", movie.rating)
-console.log("Високий рейтинг:", movie.isHighRated())
+console.log("Назва:", movie.title);
+console.log("Режисер:", movie.director);
+console.log("Рік:", movie.year);
+console.log("Рейтинг:", movie.rating);
+console.log("Високий рейтинг:", movie.isHighRated());
