4784538368de5935de8226b9155ecd13836c9dfd
Nierochron <166538574+Nierochron@users.noreply.github.com>
2025-07-10 15:03:34 +0200

    fix

--- js/script-hw15.js
+++ js/script-hw15.js
@@ -1,39 +1,24 @@
-const calculateTotalBalance = users => {
-  let sum = 0;
-  for (const user of users) {
-    sum += user.balance;
-  }
-  return sum;
-};
+const calculateTotalBalance = (users) =>
+  users.reduce((sum, user) => sum + user.balance, 0);
 
-const getUsersWithFriend = (users, friendName) => {
-  const result = [];
-  for (const user of users) {
-    if (user.friends.includes(friendName)) {
-      result.push(user.name);
-    }
-  }
-  return result;
-};
+const getUsersWithFriend = (users, friendName) =>
+  users
+    .filter((user) => user.friends.includes(friendName))
+    .map((user) => user.name);
 
-const getNamesSortedByFriendsCount = users => {
-  return users
-    .slice()
+const getNamesSortedByFriendsCount = (users) =>
+  [...users]
     .sort((a, b) => a.friends.length - b.friends.length)
-    .map(user => user.name);
-};
+    .map((user) => user.name);
 
-const getSortedUniqueSkills = users => {
-  const allSkills = [];
-  for (const user of users) {
-    allSkills.push(...user.skills);
-  }
-  const uniqueSkills = [...new Set(allSkills)];
-  return uniqueSkills.sort();
-};
+const getSortedUniqueSkills = (users) =>
+  users
+    .flatMap((user) => user.skills)
+    .filter((skill, idx, arr) => arr.indexOf(skill) === idx)
+    .sort();
 
 console.log(calculateTotalBalance(users));
-console.log(getUsersWithFriend(users, 'Briana Decker'));
-console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+console.log(getUsersWithFriend(users, "Briana Decker"));
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
 console.log(getNamesSortedByFriendsCount(users));
 console.log(getSortedUniqueSkills(users));
